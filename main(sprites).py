21853523a818298a679beb00dae9fd9b55ebfa39
Thejairex <Yairjesus777@gmail.com>
2023-04-26 19:15:55 -0300

    Testing sprite

--- main(sprites).py
+++ main(sprites).py
@@ -1,23 +1,34 @@
 # importacion de librerias
 import pygame
 import random
+import pyganim
 
-# Importacion de modulos
-class ObjetoJuego:
-    def __init__(self, width, height) -> None:
-        self.width = width
-        self.height = height
-        self.rect = pygame.Rect(0, 0, self.width, self.height)
-        
-    def draw(self, screen, color):
-        return pygame.draw.rect(screen, color, self.rect)
-class Personaje(ObjetoJuego):
+
+class Personaje(pygame.sprite.Sprite):
     def __init__(self) -> None:
-        super().__init__(50, 50)
+        super().__init__()
+        self.spritePlayer = pygame.image.load("Sprites/Player/PlayerIdle.png")
         self.speed = 5
-        self.rect.x = 10
-        self.rect.y = 10
+        frames = []
+        for i in range(4):
+            frame_x = i * 16
+            frame_y = 0
+            frame = self.spritePlayer.subsurface((frame_x, frame_y, 16, 16))
+            frames.append((frame, 100))
+
+        print(len(frames))
+        self.animation = pyganim.PygAnimation(frames)
+        self.animation.play()
+        self.animation.scale((50,50))
+        self.rect = self.animation.getRect()
         
+        
+    def draw(self, screen):
+        return self.animation.blit(screen, (100, 100))
+
+    # def draw(self):
+    #     return self.animation.play()
+
     def gameSpace(self, size):
         if self.rect.left < 0:
             self.rect.left = 0
@@ -27,44 +38,6 @@ class Personaje(ObjetoJuego):
             self.rect.top = 0
         elif self.rect.bottom > size[1]:
             self.rect.bottom = size[1]
-
-class Enemigo(ObjetoJuego):
-    def __init__(self) -> None:
-        super().__init__(15, 15)
-        self.name = "Monster"
-        self.speed = 2
-        self.rect.x = random.randint(15, 685-self.width)
-        self.rect.y = random.randint(15, 485-self.width)
-
-    def notCollision(self, others):
-        # print(others)
-        for other in others:
-            # print(other.rect)
-            if self.rect.colliderect(other.rect):
-                print("nos chocamos")
-                pass
-        
-class Arma(ObjetoJuego):
-    def __init__(self):
-        super().__init__(20, 20)
-        self.name = "Gun"
-        self.damage = 5
-        self.alive = False
-
-    def draw(self, screen, color):
-        if not self.alive:
-            self.posX = random.randint(10, 700-10)
-            self.posY = random.randint(10, 500-10)
-            self.alive = True
-            
-        self.rect = pygame.Rect(self.posX, self.posY, self.width, self.height)
-        return pygame.draw.rect(screen, color, self.rect)
-
-    def destroy(self):
-        self.width = 0
-        self.height = 0
-        self.posX = 0
-        self.posY = 0
 
 
 class Juego:
@@ -101,36 +74,37 @@ class Juego:
 
         # Entities
         self.player = Personaje()
-        self.weapon = Arma()
-        self.listEnemies = [] 
+        # self.weapon = Arma()
+        self.listEnemies = []
 
     # Generations
     def generateEntities(self):
-        self.weapon.draw(self.screen, self.colorWeapon)
-        self.player.draw(self.screen, self.white)
-        
-        while len(self.listEnemies) < self.maxEnemies:
-            self.listEnemies.append(Enemigo())
-        
-        for enemy in self.listEnemies:
-            enemy.draw(self.screen, self.blue)
-            enemy.notCollision([enemyTemp for enemyTemp in self.listEnemies if enemyTemp != enemy])
-    
+        # self.weapon.draw(self.screen, self.colorWeapon)
+        self.player.draw(self.screen)
+
+        # while len(self.listEnemies) < self.maxEnemies:
+        #     self.listEnemies.append(Enemigo())
+
+        # for enemy in self.listEnemies:
+        #     enemy.draw(self.screen, self.blue)
+        #     enemy.notCollision([enemyTemp for enemyTemp in self.listEnemies if enemyTemp != enemy])
+
     # Metodos logicos del juego
     def colliders(self):
         if self.player.rect.colliderect(self.weapon.rect):
             self.weapon.destroy()
-            
-        # Enemies 
+
+        # Enemies
         to_remove = []
         for enemy in self.listEnemies:
             if enemy.rect.colliderect(self.player.rect):
                 to_remove.append(enemy)
-        
-        self.listEnemies = [enemy for enemy in self.listEnemies if enemy not in to_remove]
+
+        self.listEnemies = [
+            enemy for enemy in self.listEnemies if enemy not in to_remove]
         self.totalEnemiesKilled += self.maxEnemies - len(self.listEnemies)
         self.enemiesKilled += self.maxEnemies - len(self.listEnemies)
-        
+
     def followPlayer(self):
         for enemy in self.listEnemies:
             if enemy.rect.x < self.player.rect.centerx:
@@ -140,13 +114,13 @@ class Juego:
             if enemy.rect.y < self.player.rect.centery:
                 enemy.rect.y += enemy.speed
             if enemy.rect.y > self.player.rect.centery:
-                enemy.rect.y -= enemy.speed 
-                
+                enemy.rect.y -= enemy.speed
+
     def upMaxEnemies(self):
         if self.enemiesKilled % 50 == 0 and self.enemiesKilled != 0:
             self.maxEnemies += 1
-            self.enemiesKilled = 0  
-        
+            self.enemiesKilled = 0
+
     def time_conversion(self):
         if self.secondsTemp >= 60:
             if self.secondsTemp % 60 == 0:
@@ -157,6 +131,7 @@ class Juego:
             return f"Tiempo: {self.secondsTemp}s"
 
     def run(self):
+        
         while not self.inGame:
             # Time Game
             self.seconds = pygame.time.get_ticks()//1000
@@ -184,7 +159,7 @@ class Juego:
                 self.inGame = True
 
             # Control de espacio del jugador
-            self.player.gameSpace(self.size)
+            # self.player.gameSpace(self.size)
 
             # Control de pantalla
             pygame.display.flip()
@@ -192,20 +167,22 @@ class Juego:
 
             # Draw Entities
             self.generateEntities()
-
-            # Logica del juego.
-            self.colliders()
-            self.followPlayer()
-            self.upMaxEnemies()
+            
+            self.player.rect.x += self.player.speed
+            # # Logica del juego.
+            # self.colliders()
+            # self.followPlayer()
+            # self.upMaxEnemies()
 
             # Texto del juego.
             labelTime = self.mainFont.render(timeConv, 0, self.white)
-            labelEnemyKilled = self.mainFont.render(f"Enemigos eliminados: {self.totalEnemiesKilled}", 0, self.white)
-            
+            labelEnemyKilled = self.mainFont.render(
+                f"Enemigos eliminados: {self.totalEnemiesKilled}", 0, self.white)
+
             self.screen.blit(labelTime,
                              (self.size[0]//2 - labelTime.get_width()//2, 0))
             self.screen.blit(labelEnemyKilled,
-                             (self.size[0]//2 - labelEnemyKilled.get_width()//2,labelTime.get_height()))
+                             (self.size[0]//2 - labelEnemyKilled.get_width()//2, labelTime.get_height()))
 
             # Fps
             self.clock.tick(self.fps)
